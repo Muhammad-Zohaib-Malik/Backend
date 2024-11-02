2bf144b8e81d19371d992348fcb960af24886121
zohaib <muhammadzohaibmalik10@gmail.com>
2024-11-02 14:02:00 +0500

    utils

--- src/index.js
+++ src/index.js
@@ -4,4 +4,12 @@ import dotenv from 'dotenv'
 
 dotenv.config({ path: "./env" })
 
-connectDB()
+connectDB().
+  then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is runnig at port : ${process.env.PORT}`)
+    })
+  })
+  .catch((err) => {
+
+  })
